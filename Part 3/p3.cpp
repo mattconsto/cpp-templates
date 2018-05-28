21c8a5d0781ddb6409e3581ff5cf5ea1d8fd0a3d
Matthew Consterdine <mrconsterdine@googlemail.com>
2018-05-28 18:06:25 +0100

    Final commit

--- Part 3/p3.cpp
+++ Part 3/p3.cpp
@@ -9,7 +9,7 @@
 int main() {
 	// Array Example
 	std::cout << "f(x) = x_{0,4} + (y_{0,5} - 2)*(z_{0,6} - 3)\n"; // The expression to be evaluated
-	typedef Add<Var<0, BOUNDS<0,4>>, Mul<Sub<Var<1, BOUNDS<0,5>>, Lit<2>>, Sub<Var<2, BOUNDS<0,6>>, Lit<3>>>> f; // Build as f
+	typedef Add<Var<BOUNDS<0,4>>, Mul<Sub<Var<BOUNDS<0,5>>, Lit<2>>, Sub<Var<BOUNDS<0,6>>, Lit<3>>>> f; // Build as f
 	std::cout << f::l << " <= f(x,y,z) <= " << f::u << "\n";
 
 	number inputs[][3] = {{0,0,0}, {1,2,3}, {4,5,6}, {7,8,9}, {10,0,0}, {0,10,0}, {-1,-2,-3}};
@@ -38,34 +38,34 @@ int main() {
 		// Evaluate f(1,2,3) and print to standard output. Unfreed inline array declarations leak 
 		// memory, used for brevity! Ideally would use variadic functions, but the spec asks for arrays.
 		std::cout << "f(1,2,3) = " << f().eval(new number[3] {1,2,3}) << "\n";
-		std::cout << "f(4,5,5) = " << f().eval(new number[3] {4,5,6}) << "\n";
+		std::cout << "f(4,5,6) = " << f().eval(new number[3] {4,5,6}) << "\n";
 		std::cout << "f(7,7,7) = " << f().eval(new number[3] {7,7,7}) << "\n\n";
 	} catch(std::out_of_range& e) {
 		std::cout << e.what() << " is out of range in f(x)\n\n";
 	}
 	
 	// Example Two
-	std::cout << "g(x) = x_{0,7} + (y_{0,7} - 2)*(x_{0,7} - 3)\n";
-	typedef Add<Var<0, BOUNDS<0,7>>, Mul<Sub<Var<1, BOUNDS<0,7>>, Lit<2>>, Sub<Var<0, BOUNDS<0,7>>, Lit<3>>>> g;
+	std::cout << "g(x,y,z) = x_{0,7} + (y_{0,7} - 2)*(z_{0,7} - 3)\n";
+	typedef Add<Var<BOUNDS<0,7>>, Mul<Sub<Var<BOUNDS<0,7>>, Lit<2>>, Sub<Var<BOUNDS<0,7>>, Lit<3>>>> g;
 	std::cout << g::l << " <= g(x,y) <= " << g::u << "\n";
 	
 	try {
-		std::cout << "g(1,2) = " << g().eval(new number[2] {1,2}) << "\n";
-		std::cout << "g(4,5) = " << g().eval(new number[2] {4,5}) << "\n";
-		std::cout << "g(7,7) = " << g().eval(new number[2] {7,7}) << "\n\n";
+		std::cout << "g(1,2,1) = " << g().eval(new number[3] {1,2,1}) << "\n";
+		std::cout << "g(4,5,4) = " << g().eval(new number[3] {4,5,4}) << "\n";
+		std::cout << "g(7,7,7) = " << g().eval(new number[3] {7,7,7}) << "\n\n";
 	} catch(std::out_of_range& e) {
 		std::cout << e.what() << " is out of range in g(x)\n\n";
 	}
 
 	// Example Three
-	std::cout << "h(x) = x_{0,3}*4 + x_{0,3}/3 - 2\n";
-	typedef Add<Mul<Var<0, BOUNDS<0,3>>, Lit<4>>, Sub<Div<Var<>, Lit<3>>, Lit<2>>> h;
+	std::cout << "h(x) = x_{0,3}*4 + y_{0,3}/3 - 2\n";
+	typedef Add<Mul<Var<BOUNDS<0,3>>, Lit<4>>, Sub<Div<Var<>, Lit<3>>, Lit<2>>> h;
 	std::cout << h::l << " <= h(x) <= " << h::u << "\n";
 
 	try {
-		std::cout << "h(1) = " << h().eval(new number[1] {1}) << "\n";
-		std::cout << "h(4) = " << h().eval(new number[1] {4}) << "\n";
-		std::cout << "h(7) = " << h().eval(new number[1] {7}) << "\n\n";
+		std::cout << "h(1,1) = " << h().eval(new number[2] {1,1}) << "\n";
+		std::cout << "h(4,4) = " << h().eval(new number[2] {4,4}) << "\n";
+		std::cout << "h(7,7) = " << h().eval(new number[2] {7,7}) << "\n\n";
 	} catch(std::out_of_range& e) {
 		std::cout << e.what() << " is out of range in h(x)\n\n";
 	}
--- Part 4/p4.h
+++ Part 4/p4.h
@@ -15,21 +15,50 @@ typedef int number; // Define a custom number data type for easy changes
 
 #define ARRAY_SIZE(array) (sizeof array / sizeof array[0]) // Macro to get size of a static array
 
+// Depth counting to auto reset the global index for multiple variables.
+struct Counter {
+	static int depth;
+	static int index;
+
+	static void descend() {
+		++depth;
+		// Automatically reset the global index when depth = 1
+		if(depth == 1) index = 0;
+	}
+	static void ascend() {--depth;}
+	static void reset() {depth = 0;}
+};
+int Counter::depth = 0;
+int Counter::index = 0;
+
 // BOUNDS template, defaults to [NUM_MIN, NUM_MAX]
 template<number L=NUM_MIN, number U=NUM_MAX> struct BOUNDS {
 	static const number l = L; static const number u = U;
 };
 
 // Multiple variable place-holder
-template<number P=0, class=void> struct Var; // Define template class
-template<number P> struct Var<P, void> { // Void case
+template<class=void> struct Var; // Define template class
+template<> struct Var<void> { // Void case
 	static const number l = NUM_MIN; static const number u = NUM_MAX;
-	static inline number eval(number* i) {return i[P];};
+	static inline number eval(number* i) {
+		Counter::descend(); // Descend 
+		number result = i[Counter::index++]; // Be careful to supply enough parameters!
+		Counter::ascend(); // Ascend
+		return result;
+	};
 };
-template<number P, number L, number U> struct Var<P, BOUNDS<L,U>> { // Bounded case
+template<number L, number U> struct Var<BOUNDS<L,U>> { // Bounded case
 	static const number l = L; static const number u = U;
 	static inline number eval(number* i) {
-		if(i[P] < L || i[P] > U) throw std::out_of_range(std::to_string(i[P])); else return i[P];
+		Counter::descend();
+		if(i[Counter::index] < L || i[Counter::index] > U) {
+			Counter::reset(); // Exceptions skip other Counter::ascend();'s, need to reset
+			throw std::out_of_range(std::to_string(i[Counter::index]));
+		} else {
+			number result = i[Counter::index++];
+			Counter::ascend();
+			return result;
+		}
 	};
 };
 
@@ -42,19 +71,39 @@ template<number N=0> struct Lit {
 // Addition, Subtraction, Multiplication, and Division binary operators
 template<class A, class B> struct Add {
 	static const number l = A::l + B::l; static const number u = A::u + B::u;
-	static inline number eval(number* i) {return A::eval(i) + B::eval(i);};
+	static inline number eval(number* i) {
+		Counter::descend();
+		number result = A::eval(i) + B::eval(i);
+		Counter::ascend();
+		return result;
+	};
 };
 template<class A, class B> struct Sub {
 	static const number l = A::l - B::u; static const number u = A::u - B::l;
-	static inline number eval(number* i) {return A::eval(i) - B::eval(i);};
+	static inline number eval(number* i) {
+		Counter::descend();
+		number result = A::eval(i) - B::eval(i);
+		Counter::ascend();
+		return result;
+	};
 };
 template<class A, class B> struct Mul {
 	static const number l = A::l * B::l; static const number u = A::u * B::u;
-	static inline number eval(number* i) {return A::eval(i) * B::eval(i);};
+	static inline number eval(number* i) {
+		Counter::descend();
+		number result = A::eval(i) * B::eval(i);
+		Counter::ascend();
+		return result;
+	};
 };
 template<class A, class B> struct Div {
 	static const number l = A::l / B::u; static const number u = A::u / B::l;
-	static inline number eval(number* i) {return A::eval(i) / B::eval(i);};
+	static inline number eval(number* i) {
+		Counter::descend();
+		number result = A::eval(i) / B::eval(i);
+		Counter::ascend();
+		return result;
+	};
 };
 
 // Conditional If template typedef, building blocks for later on
